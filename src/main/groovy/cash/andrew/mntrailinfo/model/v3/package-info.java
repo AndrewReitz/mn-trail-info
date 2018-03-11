62dbe325fd73800774bbd33e1f2b918517711024
Andrew Reitz <aj.reitz@gmail.com>
2018-03-10 21:33:01 -0600

    Apply Spotless

--- src/main/groovy/cash/andrew/mntrailinfo/model/v3/package-info.java
+++ src/main/groovy/cash/andrew/mntrailinfo/model/v3/package-info.java
@@ -1,5 +1,5 @@
 /**
- * Models that were created to provide the v3 data.
- * V3 no longer scraps the website but instead uses the new api created by MORC.
+ * Models that were created to provide the v3 data. V3 no longer scraps the website but instead uses
+ * the new api created by MORC.
  */
 package cash.andrew.mntrailinfo.model.v3;
